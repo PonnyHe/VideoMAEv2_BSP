943853ff111a83cf92c3119ace273cfd64da6ac0
PonnyHe <160093164+PonnyHe@users.noreply.github.com>
2025-11-19 16:24:28 +0800

    Update run_bspm_pretrain.sh for GPU selection

--- run_bspm_pretrain.sh
+++ run_bspm_pretrain.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-python -m torch.distributed.launch \
-  --nproc_per_node=8 \
-  main_pretrain.py \
-  --config configs/pretrain/bspm_vitb_patch522.yaml \
-  --opts \
-  MODEL.PATCH_EMBED patch_embed_bspm.PatchEmbed3D_BSPM
+GPU_ID=${1:-0}   # 默认 GPU=0，可在命令行传参
+
+CUDA_VISIBLE_DEVICES=$GPU_ID python main_pretrain.py \
+  --config bspm_vitb_patch522.yaml \
+  --opts MODEL.PATCH_EMBED patch_embed_bspm.PatchEmbed3D_BSPM
